19c76c5666ebce6b5a0814846da3d0626703f0a5
Yashas <yashaspancham@gmail.com>
2025-06-06 14:13:30 +0530

    Yashas-feat-middlewareAdded

--- src/app.js
+++ src/app.js
@@ -1,21 +1,28 @@
-const express=require("express");
+const express = require("express");
 const cors = require("cors");
-const app=express();
+const app = express();
 require("dotenv").config();
-const authLogin=require("./routes/auth");
-const entries =require("./routes/entries");
+const authLogin = require("./routes/auth");
+const entries = require("./routes/entries");
+const authenticateToken = require("./middleware/auth");
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/auth/",authLogin);
-app.use("/entries",entries);
+app.use((req, res, next) => {
+  if (req.path === "/auth/login" || req.path === "/auth/signup") return next();
+  authenticateToken(req, res, next);
+})
+
+
+app.use("/auth", authLogin);
+app.use("/entries", entries);
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+  res.send('Hello from projectyreflections!!!');
 });
 
 
-app.listen(process.env.SERVER_PORT,"0.0.0.0",()=>{
-  console.log(`Server running at http://localhost:${process.env.SERVER_PORT}`);
+app.listen(process.env.SERVER_PORT, "0.0.0.0", () => {
+  console.log(`Server running at http://localhost: ${process.env.SERVER_PORT}`);
 });

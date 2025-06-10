6cc636ba0a02a32cfcfe6f2b81c3730e6242c46b
Yashas <yashaspancham@gmail.com>
2025-06-10 17:26:25 +0530

    Yashas-feat-LongestStreakAPIDone

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,6 +1,6 @@
 const pool = require("../db");
 
-async function getUserData(req, res) {
+async function getUserName(req, res) {
     const { user_id } = req.params;
     try {
         const result = await pool.query(
@@ -18,4 +18,21 @@ async function getUserData(req, res) {
     }
 }
 
-module.exports = { getUserData };
+async function getLongestStreak(req, res) {
+    const { user_id } = req.params;
+    try {
+        const result = await pool.query(
+            "SELECT user_id, MIN(entry_date) AS streak_start, MAX(entry_date) AS streak_end, COUNT(*) AS streak_length FROM( SELECT user_id, date(created_at) AS entry_date, date(created_at) - INTERVAL '1 day' * ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY date(created_at)) AS grp FROM entries WHERE user_id = $1  GROUP BY user_id, date(created_at) ) streaks GROUP BY user_id, grp ORDER BY streak_length DESC LIMIT 1",
+            [user_id]
+        );
+        const longestStreak = (result.rows.length === 0) ? 0 : parseInt(result.rows[0].streak_length) || 0;
+        return res.status(200).json({
+            message: "Longest streak retrieved successfully",
+            longestStreak
+        });
+    } catch (error) {
+        return res.status(500).json({ message: "server error" });
+    }
+}
+
+module.exports = { getUserName, getLongestStreak };
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,8 @@
 const express=require("express");
 const router=express.Router();
-const {getUserData}=require("../controllers/users");
+const {getUserName,getLongestStreak }=require("../controllers/users");
 
-router.get("/getUserData/user_id/:user_id",getUserData);
+router.get("/getUserName/user_id/:user_id",getUserName);
+router.get("/getLongestStreak/user_id/:user_id",getLongestStreak);
 
 module.exports=router;
